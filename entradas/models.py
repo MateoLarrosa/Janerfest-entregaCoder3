a2f262b938de9e1bf584746c0b47ad1632aa98cf
Mateo C. Larrosa <mateoclarrosa@gmail.com>
2023-04-06 22:58:23 -0300

    Ultimo commit para seguir desde la otra compu

--- entradas/models.py
+++ entradas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=20)
+    mail = models.CharField(max_length=30,)
+    cant_entradas = models.IntegerField()
